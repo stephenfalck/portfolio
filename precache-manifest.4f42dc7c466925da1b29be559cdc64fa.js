34c9076983d07d66914f01cee4c1015b3b0767f2
Stephen Falck <stephenvfalck@gmail.com>
2020-03-22 21:08:30 +0000

    Updates

---
+++ precache-manifest.4f42dc7c466925da1b29be559cdc64fa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fb33d399c09aefc59954b115d2beb1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f84869585c7a95ee2d43",
+    "url": "/portfolio/static/css/main.4ef0f435.chunk.css"
+  },
+  {
+    "revision": "b4ab03754d1ae817df5e",
+    "url": "/portfolio/static/js/2.b24d7a65.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/portfolio/static/js/2.b24d7a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84869585c7a95ee2d43",
+    "url": "/portfolio/static/js/main.32cb8656.chunk.js"
+  },
+  {
+    "revision": "46f351f7308d95ad02f6",
+    "url": "/portfolio/static/js/runtime-main.32ff3a7f.js"
+  },
+  {
+    "revision": "c904e7d16a7b800f55a6cb6932af0eef",
+    "url": "/portfolio/static/media/Screenshot 2020-03-01 at 16.44.12.c904e7d1.png"
+  },
+  {
+    "revision": "775cfa933eacbcf37fb9c595e662294a",
+    "url": "/portfolio/static/media/Screenshot 2020-03-01 at 19.56.33.775cfa93.png"
+  },
+  {
+    "revision": "08b21e94aec3f4ad1facee58d71bc51d",
+    "url": "/portfolio/static/media/image_original (1).08b21e94.jpg"
+  },
+  {
+    "revision": "a9e499461b536b9a9aca307917339f27",
+    "url": "/portfolio/static/media/image_original (1)_KH.a9e49946.jpg"
+  },
+  {
+    "revision": "3cf3db5001d083b4d739139c9a69f8b3",
+    "url": "/portfolio/static/media/image_original (2).3cf3db50.jpg"
+  },
+  {
+    "revision": "cc4f5ab0103ace61e57aee7031a68214",
+    "url": "/portfolio/static/media/image_original (2)_KH.cc4f5ab0.jpg"
+  },
+  {
+    "revision": "cbe9210a71a623d73cf516612a42d027",
+    "url": "/portfolio/static/media/image_original (3).cbe9210a.jpg"
+  },
+  {
+    "revision": "ffcf770f9d74688da820b7b27da157a1",
+    "url": "/portfolio/static/media/image_original (3)_KH.ffcf770f.jpg"
+  },
+  {
+    "revision": "352aa9101f56092a50b2dbe41182a18e",
+    "url": "/portfolio/static/media/image_original.352aa910.jpg"
+  },
+  {
+    "revision": "78c1243b43c83cfd3a5ac58714d02b56",
+    "url": "/portfolio/static/media/image_original_KH.78c1243b.jpg"
+  },
+  {
+    "revision": "ce06fd59cc06c725058878c241010b0e",
+    "url": "/portfolio/static/media/jc-gellidon-EH9f0TI5wco-unsplash_cropped.ce06fd59.jpg"
+  }
+]);
